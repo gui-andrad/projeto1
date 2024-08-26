9bf5fa4abd4aff40f3b216c69c42d87ebb1b855c
gui-andrad <devluizandrade@gmail.com>
2024-08-25 23:54:27 -0300

    FEAT: adicionando o formulário e mudando o layout

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import express from 'express'
+import * as express from 'express'
 
 const app = express();
 
 const videoYtbLazy = document.getElementById('videoYtb1970');
 
-(videoYtbLazy as HTMLElement)?.addEventListener('click', snippetYtbLazyPlay);
+videoYtbLazy.addEventListener('click', snippetYtbLazyPlay);
 
 function snippetYtbLazyPlay(evt: Event){
-    const target = evt.currentTarget as HTMLElement;
-    target.innerHTML = <iframe src="https://www.youtube.com/embed/${target.dataset.video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>;
+    evt.currentTarget.removeEventListener('click', snippetYtbLazyPlay);
 
+    evt.currentTarget.innerHTML = `<iframe src="https://www.youtube.com/embed/${evt.currentTarget.dataset.id}?autoplay=1"
+     title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+      picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
 }
 
 app.listen(3333);
